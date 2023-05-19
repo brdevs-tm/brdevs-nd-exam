7f0d6f0374014f1da4e84e17e2a93aef16d0ed8d
khamidov <bright19.off@gmail.com>
2023-05-19 15:22:34 +0500

    main

--- js/carousel.js
+++ js/carousel.js
@@ -17,3 +17,22 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+// $('.owl-carousel-2').owlCarousel({
+//     loop:true,
+//     margin:20,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         400: {
+//             items: 2
+//         },
+//         900:{
+//             items:3
+//         },
+//         1000:{
+//             items:3
+//         }
+//     }
+// })
